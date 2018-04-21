e95a21863ed2b2d7ee5d84cb26fa17aae123ad3b
omniomi <omniomi@gmail.com>
2018-04-20 20:10:42 -0600

    Build files, analyzer settings, vscode files.

--- tests/shared.ps1
+++ tests/shared.ps1
@@ -5,7 +5,7 @@ $ModuleManifestPath = "$PSScriptRoot\..\src\$ModuleManifestName"
 
 if (!$SuppressImportModule) {
     # -Scope Global is needed when running tests from inside of psake, otherwise
-    # the module's functions cannot be found in the PSMailTools\ namespace
+    # the module's functions cannot be found in the Grouper\ namespace
     Import-Module $ModuleManifestPath -Scope Global
 }
 
